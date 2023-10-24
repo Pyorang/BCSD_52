64c94b04678d7f57dc8559f29d015c13ae00aa8a
Pyorang <sudangi143@gmail.com>
2023-10-24 21:27:59 +0900

    Lesson_27

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public TalkManager talkManager;
+    public QuestManager questManager;
+    public Text talkText;
+    public Image portraitImg;
+    public GameObject scanObject;
+    public GameObject talkPanel;
+    public bool isAction;
+    public int talkIndex;
+
+    void Start()
+    {
+        Debug.Log(questManager.CheckQuest());
+    }
+
+    public void Action(GameObject scanObj)
+    {
+        scanObject = scanObj;
+        ObjtData objData = scanObject.GetComponent<ObjtData>();
+        Talk(objData.id, objData.isNpc);
+        talkPanel.SetActive(isAction);
+    }
+
+    void Talk(int id, bool isNpc)
+    {
+        int questTalkIndex = questManager.GetQuestTalkIndex(id);
+        string talkData = talkManager.GetTalk(id +questTalkIndex , talkIndex);
+
+        if (talkData == null)
+        {
+            isAction = false;
+            talkIndex = 0;
+            Debug.Log(questManager.CheckQuest(id));
+            return;
+        }
+
+        if (isNpc)
+        {
+            talkText.text = talkData.Split(':')[0];
+
+            portraitImg.sprite = talkManager.GetPortrait(id, int.Parse(talkData.Split(':')[1]));
+            portraitImg.color = new Color(1, 1, 1, 1);
+        }
+
+        else
+        {
+            talkText.text = talkData;
+
+            portraitImg.color = new Color(1, 1, 1, 0);
+        }
+
+        isAction = true;
+        talkIndex++;
+    }
+}
