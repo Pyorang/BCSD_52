239b559faf74bbc095f25fe5619cf80b046fca99
Pyorang <sudangi143@gmail.com>
2023-10-27 23:01:52 +0900

    Lesson_30

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -31,17 +31,18 @@ public class GameManager : MonoBehaviour
         //Sub Menu
         if (Input.GetButtonDown("Cancel"))
         {
-            if (menuSet.activeSelf)
-            {
-                menuSet.SetActive(false);
-            }
-            else
-            {
-                menuSet.SetActive(true);
-            }
+            SubMenuActive();
         }
 
 
+    }
+
+    public void SubMenuActive()
+    {
+        if (menuSet.activeSelf)
+        {
+            menuSet.SetActive(false);
+        }
     }
 
     public void Action(GameObject scanObj)
